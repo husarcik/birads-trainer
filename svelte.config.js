365ae7f6a528cb79fd5dc264d2780086a41ed624
husarcik <edward.husarcik@gmail.com>
2024-10-16 09:12:44 -0500

    setup github actions

--- svelte.config.js
+++ svelte.config.js
@@ -21,10 +21,13 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
 			precompress: false,
-			strict: true
-		})
+			strict: true,
+			fallback: '404.html',
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
